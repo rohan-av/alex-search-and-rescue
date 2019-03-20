5b6d34d71c43eacc8c44b6946fe0f8a2bba4a357
CheeSengg <42791836+CheeSengg@users.noreply.github.com>
2019-03-20 09:25:53 +0800

    W9S2

---
+++ w8s2/constants.h
@@ -0,0 +1,46 @@
+#ifndef __CONSTANTS_INC__
+#define __CONSTANTS_INC__
+
+/* 
+ *  This file containts all the packet types, commands
+ *  and status constants
+ *  
+ */
+
+// Packet types
+typedef enum
+{
+  PACKET_TYPE_COMMAND = 0,
+  PACKET_TYPE_RESPONSE = 1,
+  PACKET_TYPE_ERROR = 2,
+  PACKET_TYPE_MESSAGE = 3,
+  PACKET_TYPE_HELLO = 4
+} TPacketType;
+
+// Response types. This goes into the command field
+typedef enum
+{
+  RESP_OK = 0,
+  RESP_STATUS=1,
+  RESP_BAD_PACKET = 2,
+  RESP_BAD_CHECKSUM = 3,
+  RESP_BAD_COMMAND = 4,
+  RESP_BAD_RESPONSE = 5 
+} TResponseType;
+
+
+// Commands
+// For direction commands, param[0] = distance in cm to move
+// param[1] = speed
+typedef enum
+{
+  COMMAND_FORWARD = 0,
+  COMMAND_REVERSE = 1,
+  COMMAND_TURN_LEFT = 2,
+  COMMAND_TURN_RIGHT = 3,
+  COMMAND_STOP = 4,
+  COMMAND_GET_STATS = 5,
+  COMMAND_CLEAR_STATS = 6
+} TCommandType;
+#endif
+
